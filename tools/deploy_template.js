40d76a91e40d6f6be3df3ea46d9d45977dd7d95f
GuillaumeCisco <guillaumecisco@gmail.com>
2018-02-13 18:49:53 +0100

    Fix deploy_template

--- tools/deploy_template.js
+++ tools/deploy_template.js
@@ -10,5 +10,5 @@ const redis_port = 6379;
 
 console.log(`Deploying ${registry}/${name}:${timestamp}`);
 
-shell.exec(`docker build --build-arg raven_url=${raven_url} redis_host=${redis_host} redis_port=${redis_port} -t ${registry}/${name}:${timestamp} -t ${registry}/${name}:latest .`);
+shell.exec(`docker build --build-arg raven_url=${raven_url} --build-arg redis_host=${redis_host} --build-arg redis_port=${redis_port} -t ${registry}/${name}:${timestamp} -t ${registry}/${name}:latest .`);
 shell.exec(`docker push ${registry}/${name}:latest`);
