b90fc3f2833d495ccc1b29efb8ac0699588006c4
GuillaumeCisco <guillaumecisco@gmail.com>
2019-12-20 15:17:49 +0100

    update yarn.lock

--- test/setup.js
+++ test/setup.js
@@ -2,7 +2,7 @@
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import serializer from 'jest-glamor-react'
+import serializer from 'jest-glamor-react';
 import {sheet} from 'emotion';
 
 
@@ -12,8 +12,7 @@ configure({adapter: new Adapter()});
 // Add css class to snapshots
 /** Change the class name to emotion-xxx instead of glamor-xxx */
 function replaceClassNames(className, index) {
-    return `emotion-${index}`
+    return `emotion-${index}`;
 }
 
 expect.addSnapshotSerializer(serializer(sheet, replaceClassNames));
-
