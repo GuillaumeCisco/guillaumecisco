ec889c762447030129e0c387037e67f6c8da7623
Guillaume Cisco <guillaumecisco@gmail.com>
2026-03-08 23:28:39 +0100

    fix

--- src/server/service-worker.js
+++ src/server/service-worker.js
@@ -1,32 +1,23 @@
 import { clientsClaim } from 'workbox-core';
-import { precacheAndRoute, matchPrecache } from 'workbox-precaching';
-import { registerRoute, setCatchHandler, setDefaultHandler } from 'workbox-routing';
-import { CacheFirst, NetworkOnly } from 'workbox-strategies';
-
-console.log('SW VERSION OFFLINE V2 DIST WEB');
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
 
 clientsClaim();
 self.skipWaiting();
 
 precacheAndRoute(self.__WB_MANIFEST || []);
 
-// Toutes les navigations tentent le réseau.
-// Si ça échoue, setCatchHandler prend la main.
-setDefaultHandler(new NetworkOnly());
-
-setCatchHandler(async ({ event }) => {
-    if (event.request.destination === 'document') {
-        return (await matchPrecache('/dist/web/offline.html')) || Response.error();
-    }
-    return Response.error();
-});
-
 registerRoute(
     ({ request }) => request.destination === 'image',
-    new CacheFirst({ cacheName: 'images' })
+    new CacheFirst({
+        cacheName: 'images',
+    })
 );
 
 registerRoute(
     ({ request }) => request.destination === 'font',
-    new CacheFirst({ cacheName: 'fonts' })
+    new CacheFirst({
+        cacheName: 'fonts',
+    })
 );
